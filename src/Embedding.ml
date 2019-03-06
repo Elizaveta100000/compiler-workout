da30441853f443203738429ed97652e6533b3fa0
Елизавета Турчина <32576372+Elizaveta100000@users.noreply.github.com>
2019-03-07 01:35:19 +0300

    Update Embedding.ml

--- src/Embedding.ml
+++ src/Embedding.ml
@@ -1,17 +1,36 @@
+Skip to content
+ 
+Search or jump to…
+
+Pull requests
+Issues
+Marketplace
+Explore
+ @Elizaveta100000 Sign out
+0
+0 84 Elizaveta100000/compiler-workout
+forked from danyaberezun/compiler-workout
+ Code  Pull requests 0  Projects 0  Wiki  Insights  Settings
+compiler-workout/src/Embedding.ml
+@Elizaveta100000 Elizaveta100000 Update Embedding.ml
+81050ed  13 days ago
+@Elizaveta100000 @danyaberezun
+42 lines (32 sloc)  941 Bytes
+    
 (* A deep embedding of simple expressions in OCaml. *)
 
 (* Opening GT yet again. *)
 open GT
        
 (* Opening the substrate module for convenience. *)
-open Syntax
+open Expr
 
 (* Shortcuts for leaf constructors *)
-let ( ! ) x = Expr.Var x
-let ( !? ) n = Expr.Const n
+let ( ! ) x = Var x
+let ( !? ) n = Const n
 
 (* Implementation of operators *)
-let binop op x y = Expr.Binop (op, x, y)
+let binop op x y = Binop (op, x, y)
                          
 let ( +  ) = binop "+"
 let ( -  ) = binop "-"
@@ -27,15 +46,16 @@ let ( != ) = binop "!="
 let ( && ) = binop "&&"
 let ( || ) = binop "!!"
 
-let ( =:= ) x e = Stmt.Assign (x, e)
-let read  x = Stmt.Read x
-let write e = Stmt.Write e
-let (|>) x y = Stmt.Seq (x, y)
-                             
 (* Some predefined names for variables *)
 let x = !"x"
 let y = !"y"
 let z = !"z"
 let t = !"t"
+  (* Voila; comment this out before submitting the solution
+let _ =
+List.iter (fun e -> Printf.printf "eval s (%s) = %d\n" (show(expr) e) (eval s e)) [x+y*z- !?3; t-z+y && x]
+*)
+
 
                    
+
