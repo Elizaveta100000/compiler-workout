b202e97b351d2c284a600a24e9a3ee2de253d669
Елизавета Турчина <32576372+Elizaveta100000@users.noreply.github.com>
2019-03-07 02:07:40 +0300

    Update SM.ml

--- src/SM.ml
+++ src/SM.ml
@@ -1,15 +1,29 @@
 open GT       
+       
+(* The type for the stack machine instructions *)
+@type insn =
+(* binary operator                 *) | BINOP of string
+(* put a constant on the stack     *) | CONST of int                 
+(* read to stack                   *) | READ
+(* write from stack                *) | WRITE
+(* load a variable to the stack    *) | LD    of string
+(* store a variable from the stack *) | ST    of string with show
 
+(* The type for the stack machine program *)                                                               
+type prg = insn list
 
- open List
+(* The type for the stack machine configuration: a stack and a configuration from statement
+   interpreter
+ *)
+type config = int list * Syntax.Stmt.config
 
- (* The type for the stack machine instructions *)	(* The type for the stack machine instructions *)
-@type insn =	@type insn =
-(* binary operator                 *) | BINOP of string	(* binary operator                 *) | BINOP of string
-@@ -23,7 +25,17 @@ type config = int list * Syntax.Stmt.config
-    Takes a configuration and a program, and returns a configuration as a result	   Takes a configuration and a program, and returns a configuration as a result
- *)                         	 *)                         
-let eval _ = failwith "Not yet implemented"	 let rec eval cfg prg =
+(* Stack machine interpreter
+
+     val eval : config -> prg -> config
+
+   Takes a configuration and a program, and returns a configuration as a result
+ *)                         
+let rec eval cfg prg =
   let step (st, (s, i, o)) p = match p with
     | BINOP op -> (Syntax.Expr.operation op (hd (tl st)) (hd st) :: (tl (tl st)), (s, i, o))
     | CONST n  -> (n :: st, (s, i, o))
@@ -21,14 +35,23 @@ let eval _ = failwith "Not yet implemented"	 let rec eval cfg prg =
     | [] -> cfg
     | p :: ps -> eval (step cfg p) ps
 
+(* Top-level evaluation
 
- (* Top-level evaluation	(* Top-level evaluation
- @@ -41,4 +53,13 @@ let run i p = let (_, (_, _, o)) = eval ([], (Syntax.Expr.empty, i, [])) p in o
-   stack machine	   stack machine
- *)	 *)
+     val run : int list -> prg -> int list
 
+   Takes an input stream, a program, and returns an output stream this program calculates
+*)
+let run i p = let (_, (_, _, o)) = eval ([], (Syntax.Expr.empty, i, [])) p in o
 
- let compile _ = failwith "Not yet implemented"	let rec compile_expr e = match e with
+(* Stack machine compiler
+
+     val compile : Syntax.Stmt.t -> prg
+
+   Takes a program in the source language and returns an equivalent program for the
+   stack machine
+ *)
+
+let rec compile_expr e = match e with
     | Syntax.Expr.Const n -> [CONST n]
     | Syntax.Expr.Var v -> [LD v]
     | Syntax.Expr.Binop (op, l_e,r_e) -> compile_expr l_e@ compile_expr r_e@ [BINOP op]
